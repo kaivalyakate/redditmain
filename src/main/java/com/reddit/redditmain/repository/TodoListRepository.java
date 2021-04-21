4cdeded137177138798097fdab25556721537b37
Kaivalya Kate <6522kaivalya@gmail.com>
2021-04-21 10:24:58 +0530

    Pushing

--- src/main/java/com/reddit/redditmain/repository/TodoListRepository.java
+++ src/main/java/com/reddit/redditmain/repository/TodoListRepository.java
@@ -6,5 +6,5 @@ import com.reddit.redditmain.dto.*;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TodoListRepository extends MongoRepository<Task, String> {
-
+    
 }
